65c5bfbac5b24d89df8e52e7c19832d009d8adca
hoai tran <tranngochoai02051999@gmail.com>
2023-07-25 14:46:47 +0700

    [FEATURE] Add header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="grid grid-cols-12 my-[30px] md:py-[100px]">
+      <div className="col-span-8 pr-[100px]">
+        <h2 className="text-3xl font-bold">I am Hoai!</h2>
+        <p>
+          I&rsquo;m a full-stack developer (Vue.js, React.js & Node.js). I am
+          not satisfied with my skill and working hard towards a higher skill. I
+          have been developing web applications for over 4 years. I aim to make
+          high-quality works with 100% effort for all customers.
+        </p>
+        <div className="flex mt-10 gap-x-5">
+          <div>
+            <a href="https://github.com/hoai-tn" target="_blank">
+              <Image
+                className="rounded-full"
+                src="/images/github.svg"
+                width={30}
+                height={30}
+                alt="github"
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://join.skype.com/invite/w5sgObSf7lGV"
+              target="_blank"
+            >
+              <Image
+                className="rounded-full"
+                src="/images/skype.svg"
+                width={30}
+                height={30}
+                alt="close"
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/ho%C3%A0i-tr%E1%BA%A7n-878b7a244/"
+              target="_blank"
+            >
+              <Image
+                className="rounded-full"
+                src="/images/in.svg"
+                width={30}
+                height={30}
+                alt="close"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-4">
+        <div className="mx-auto flex">
+          <div className=" w-[200px] h-[200px] rounded-full bg-gradient-to-b from-pink-500 to-blue-500 p-1">
+            <Image
+              className="rounded-full"
+              src="/images/avatar.jpg"
+              width={200}
+              height={200}
+              alt="close"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
