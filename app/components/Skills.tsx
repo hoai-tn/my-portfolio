180f843c877d7c7275b9572c503f43133e5719b5
hoai tran <tranngochoai02051999@gmail.com>
2023-07-26 16:03:37 +0700

    [FEATURE] Completed skills component

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -4,24 +4,24 @@ import React from "react";
 const Skills = () => {
   const skills = [
     { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
-    { image: "js.svg", text: "javascript" },
+    { image: "icon-typescript.svg", text: "Typescript" },
+    { image: "vue.svg", text: "Vue.js" },
+    { image: "icon-react.svg", text: "React.js" },
+    { image: "Next.svg", text: "Next.js" },
+    { image: "nuxt.svg", text: "Nuxt.js" },
+    { image: "pinia.svg", text: "Pinia" },
+    { image: "icon-sass.svg", text: "Sass" },
+    { image: "icon-tailwindcss.svg", text: "Tailwindcss" },
+    { image: "icon-nodejs.svg", text: "Node.js" },
+    { image: "icon-express.svg", text: "Express.js" },
+    { image: "icon-mongodb.svg", text: "MongoDB" },
+    { image: "mysql.svg", text: "MySql" },
+    { image: "icon-cypress.svg", text: "Cypress" },
+    { image: "jest.svg", text: "Jest.js" },
+    { image: "icon-git.svg", text: "Git" },
   ];
   return (
-    <section>
+    <section id="tech-stack" className="mt-20">
       <div className="text-center">
         <h2 className="text-2xl">My Tech Stack</h2>
         <p className="italic">
@@ -32,12 +32,14 @@ const Skills = () => {
         {skills.map((item, index: Number) => (
           <div key={index} className="mx-auto text-center">
             <Image
+            className="hover:scale-110 transition-all"
+              style={{ height: 90 }}
               src={`/images/${item.image}`}
               width={90}
               height={90}
               alt="javascript"
             />
-            <p>{item.text}</p>
+            <p className="mt-2">{item.text}</p>
           </div>
         ))}
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
+import AboutMe from "./components/AboutMe";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <main className="container mx-auto px-2">
         <Header/>
+        <AboutMe/>
         <Skills/>
       </main>
     </div>
